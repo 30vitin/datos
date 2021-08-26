ad8f0d0c672aa280008f685906a22e545630f2a1
30vitin <71181298+30vitin@users.noreply.github.com>
2021-08-25 20:21:23 -0500

    cc

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '762512805518-dhsugh7l0fkrqp1b48o7elvnko452pr6.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', '280P11DT35W3mUadnPM49QLG');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://127.0.0.1:8080/auth/callback');
+
+?>
